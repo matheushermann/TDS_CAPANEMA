ba10695c71324a49fb4357ad600fc1220a8a80b4
matheushermann <matheushermann0@gmail.com>
2026-05-04 21:54:36 -0300

    Operadores em JS

---
+++ operadores/desafioOperadores.js
@@ -0,0 +1,32 @@
+//DESAFIO
+
+/* 
+ATALHO: SHIFT + ALT + A
+
+01 Utilizando os operadores relacionados, declare 2 variáveis e 
+imprima no output/console 5 valores TRUE e 5 valores FALSE
+
+*/
+
+let a = 66; // Number
+let b = "70"; // String
+let c = "240"; // String
+
+
+
+console.log(b > b);
+console.log(c < a);
+console.log(a == b); 
+console.log(a === b); 
+console.log(b === c); 
+console.log(c > a);
+console.log(a < b);
+console.log(a != b); 
+console.log(a !== b); 
+console.log(b !== c); 
+
+// valores TRUE
+
+
+// Valores FALSE
+
---
+++ operadores/logicos.js
@@ -0,0 +1,11 @@
+let temDinheiro = true
+let estaChovendo = false
+let carroEstaNaGaragem = true;
+
+let logicaAnd = "#AND Voce vai ao cinema?";
+logicaAnd += temDinheiro && estaChovendo;
+console.log(logicaAnd);
+
+let logicaOr = "#OR Voce vai ao cinema?";
+logicaOr += estaChovendo || carroEstaNaGaragem
+console.log(logicaOr);
